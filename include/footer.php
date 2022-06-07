ecfe5e71d405d5601509eda0336f37045c98d0a2
sane cheikh oumar <safietou2717@gmail.com>
2022-06-07 14:34:33 +0200

    ajouter footer

---
+++ include/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>&copy; <?php echo date("y");?>2022-ceppic corporation</p>
+
+</footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+date_default_timezone_set('Europe/paris');
+
+
+require_once './include/footer.php';
+require_once './include/footer.php';
+require_once './include/footer.php';
+
+?>
