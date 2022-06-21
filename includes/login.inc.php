a35c6925f003e0d471012d9343e8d49022dadde6
sane cheikh oumar <safietou2717@gmail.com>
2022-06-21 17:00:45 +0200

    ajouter authentification

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -26,10 +26,38 @@ if (isset($_POST['frmlogin'])) {
         echo $messageErreur;
 
         include './include/frmLogin.php';
-    } else {
+    } 
+    else {
+        $requeteLogin= "SELECT password FROM utilisateur WHERE mail='$email'";
+        $sqlLogin = new Sql();
+        $resultatLogin = $sqlLogin->Lister($requeteLogin);
+        if (count($resultatLogin) >0) {
+            //tratement pour verifier le mot de passe
+            $resultatLoginPassworld = $resultatLogin[0]['password'];
+            if (Password_verify($password  ,$resultatLoginPassword)){
+                $message ='vous etes connecte';
+                $_SESSION['Login']=true;
+            }else{
+                $message = "Erreur d'authentication";
+                $_SESSION = false;
+            }
+
+        }else{
+            $message ="Votre adress n'est pas dans la base";
+        }
+        echo $message;
+        $url = $_SERVER['HTTP_ORIGIN'].dirname($_SERVER['REQUEST_URL']);
+        echo redirection($url, 2000);
+        echo "<p><a href=\"$url\">Revenir à la page d'accueil</a></p>";
+
         // include './include/frmLogin.php';
         $_SESSION['LoginUser'] = $email;
-        header('location:index.php?membre');
+        $messageEmail = $email . ' vous êtes connecté !';
+
+        sendEmail($email, 'contact@ceppic-php.fr', 'Login Success', $messageEmail);
+
+        header('Location:index.php?page=admin');
+       
     }
 } else {
     $email = "";
